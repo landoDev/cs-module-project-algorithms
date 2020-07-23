6f29c51c5ff335d8e1d15c3f76f1bd16579ab364
Landon Turner <lturner@teamworks.com>
2020-07-22 22:17:03 -0400

    passing most of cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -7,7 +7,21 @@ def eating_cookies(n):
     # use recursion to make a binary tree
     # the amount of cookies has an index ( is a list )
     # I can eat the index from left to right, call eat cookies recursively until there are no cookies left
-    pass
+    
+    # check how many ways you can eat the cookie
+    # check out how many he ate
+    # if n less than equal to 0
+    if n <= 0:
+        return 1
+    # if n less than equal to 0
+    if n == 1: 
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 4
+    # recursively return each subtree
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
