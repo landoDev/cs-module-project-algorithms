ee32e6aa492e61592800cc7f5ec9d3dad9d8a815
Landon Turner <lturner@teamworks.com>
2020-07-23 13:16:33 -0400

    guided cookies added

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,26 +2,48 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
-    # use recursion to make a binary tree
-    # the amount of cookies has an index ( is a list )
-    # I can eat the index from left to right, call eat cookies recursively until there are no cookies left
-    
-    # check how many ways you can eat the cookie
-    # check out how many he ate
-    # if n less than equal to 0
-    if n <= 0:
+# def eating_cookies(n):
+    # # Your code here
+    # # check how many ways you can eat the cookie
+    # # check out how many he ate
+    # # small solution
+    # # if n == 0:
+    # #     return 1
+    # # if n < 0:
+    # #     return 0
+    #     # big solution
+    # # if n less than equal to 0
+    # if n <= 0:
+    #     return 1
+    # # if n less than equal to 0
+    # if n == 1: 
+    #     return 1
+    # if n == 2:
+    #     return 2
+    # if n == 3:
+    #     return 4
+    # # recursively return each subtree
+    # return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
+def eating_cookies(n, cache=None):
+    # base cases
+    if n < 0:
+        return 0
+    elif n == 0:
         return 1
-    # if n less than equal to 0
-    if n == 1: 
-        return 1
-    if n == 2:
-        return 2
-    if n == 3:
-        return 4
-    # recursively return each subtree
-    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    # check if this has been calculated by looking in the cache
+    elif cache is not None and cache[n] > 0:
+        # 2nd base case
+        # return the already computed answer and don't recurse
+        return cache[n]
+    else:
+        # have to store value of call expressions in the cache
+        if cache is None:
+            # initalize empty list for a cache
+            cache = [0 for i in range(n+1)]
+            # could also do a dictionary
+        cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)  
+    return cache[n]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
