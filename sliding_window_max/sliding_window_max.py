8191ea0073bf9bd5b760e4aed42bd66f86e75dc7
Landon Turner <lturner@teamworks.com>
2020-07-22 21:58:18 -0400

    passing sliding window

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,8 +4,26 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
-    # this doesn't make sense, looking at the test. I do not understand this problem and what it is asking
-    pass
+    # arr 
+    # initiate largest values
+    largest = []
+    # start index
+    start = 0 
+    # the end = k
+    end = k
+    # while the end - 1 < than length of nums
+    while end - 1 < len(nums):
+        #current values = # nums[start index: end index]
+        current = nums[start:end]
+        # call max for largest values
+        largest.append(max(current))
+        # increment indices by 1
+        start += 1
+        end +=1
+    # return largest values
+    return largest
+
+
 
 
 if __name__ == '__main__':
